54d29d0e06632caf1c255f81b94db96492b4fc95
Clark Miller <glitchclick@gmail.com>
2021-02-19 15:02:36 -0500

    Add project files.

---
+++ Part 3 - Chapter Problem/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace Part_3___Chapter_Problem
+{
+    partial class BasicInput
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.lblHeight = new System.Windows.Forms.Label();
+            this.btnResults = new System.Windows.Forms.Button();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(13, 13);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 20);
+            this.txtName.TabIndex = 0;
+            this.txtName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(13, 39);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(100, 20);
+            this.txtAge.TabIndex = 1;
+            this.txtAge.TextChanged += new System.EventHandler(this.txtAge_TextChanged);
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(13, 65);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(100, 20);
+            this.txtHeight.TabIndex = 2;
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(119, 16);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(63, 13);
+            this.lblName.TabIndex = 3;
+            this.lblName.Text = "Enter Name";
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Location = new System.Drawing.Point(119, 42);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(54, 13);
+            this.lblAge.TabIndex = 4;
+            this.lblAge.Text = "Enter Age";
+            // 
+            // lblHeight
+            // 
+            this.lblHeight.AutoSize = true;
+            this.lblHeight.Location = new System.Drawing.Point(119, 68);
+            this.lblHeight.Name = "lblHeight";
+            this.lblHeight.Size = new System.Drawing.Size(83, 13);
+            this.lblHeight.TabIndex = 5;
+            this.lblHeight.Text = "Enter Height (m)";
+            // 
+            // btnResults
+            // 
+            this.btnResults.Location = new System.Drawing.Point(13, 91);
+            this.btnResults.Name = "btnResults";
+            this.btnResults.Size = new System.Drawing.Size(75, 23);
+            this.btnResults.TabIndex = 6;
+            this.btnResults.Text = "Results";
+            this.btnResults.UseVisualStyleBackColor = true;
+            this.btnResults.Click += new System.EventHandler(this.btnResults_Click);
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(193, 42);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(42, 13);
+            this.lblMessage.TabIndex = 7;
+            this.lblMessage.Text = "Results";
+            // 
+            // BasicInput
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.btnResults);
+            this.Controls.Add(this.lblHeight);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtAge);
+            this.Controls.Add(this.txtName);
+            this.Name = "BasicInput";
+            this.Text = "Basic Input";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAge;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAge;
+        private System.Windows.Forms.Label lblHeight;
+        private System.Windows.Forms.Button btnResults;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ Part 3 - Chapter Problem/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Part_3___Chapter_Problem
+{
+    public partial class BasicInput : Form
+    {
+        string name;
+        string age;
+        string height;
+        public BasicInput()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            name = txtName.Text;
+        }
+
+        private void txtAge_TextChanged(object sender, EventArgs e)
+        {
+            age = txtAge.Text;
+        }
+
+        private void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+            height = txtHeight.Text;
+        }
+
+        private void btnResults_Click(object sender, EventArgs e)
+        {
+            int lifeExp = System.Convert.ToInt32(age);
+            double dblHeight = System.Convert.ToDouble(height);
+            lblMessage.Text = ($"Hello {name}, you have {82 - lifeExp} years left until you die. You are {2.72 - dblHeight} m shorter than the tallest person in the world ");
+        }
+    }
+}
